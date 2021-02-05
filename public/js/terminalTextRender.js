f8808a0e2d68073b702b3c4c91de5ac368e3909b
burritoOverflow <47379530+burritoOverflow@users.noreply.github.com>
2021-02-04 22:28:14 -0500

    We'll do this with a grid layout.

--- public/js/terminalTextRender.js
+++ public/js/terminalTextRender.js
@@ -6,11 +6,11 @@
 (() => {
   let counter = 0;
   // get the room from the query string parameters
-  const message = `Welcome to ${
-    window.location.search.split('&')[1].split('=')[1]
-  }`;
+  const message = `Welcome to ${window.location.search.split('&')[1].split('=')[1]}`;
   const typeWriterEl = document.getElementById('main-header');
   const inputTextArea = document.getElementById('message-text');
+
+  inputTextArea.style.height = `${document.getElementById('message-thread').clientHeight * 0.75}px`;
 
   const durationBetChar = 100;
 
@@ -22,9 +22,7 @@
       ++counter;
       // dynamically resize the textarea to correspond to
       // the growth of the header area
-      inputTextArea.style.width = `${
-        document.getElementById('heading').clientWidth
-      }px`;
+
       setTimeout(terminalTextRender, durationBetChar);
     }
   };
