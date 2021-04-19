59b04e15c485a7e18f093f300a30c07349ef3026
burritoOverflow <47379530+burritoOverflow@users.noreply.github.com>
2021-04-18 23:41:03 -0400

    scroll update

--- public/js/subscribeUpdates.js
+++ public/js/subscribeUpdates.js
@@ -5,4 +5,6 @@ eventSource.addEventListener('message', (e) => {
   const li = document.createElement('li');
   li.innerText = `${new Date().toLocaleString()} - ${e.data}`;
   updateList.appendChild(li);
+  // scroll the latest into view
+  updateList.scrollTop = updateList.scrollHeight - updateList.clientHeight;
 });
